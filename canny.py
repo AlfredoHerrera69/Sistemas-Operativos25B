59abfe46d371af7fc65224e5030e07810aa1349c
root <root@Redes08.localdomain>
2025-08-22 16:23:05 -0600

    Subo código e imagen

---
+++ canny.py
@@ -0,0 +1,18 @@
+import cv2
+
+# Leer la imagen (asegúrate de que el nombre sea exacto)
+img = cv2.imread('cartas.jpeg', 0)
+
+if img is None:
+    print("Error: no se pudo cargar la imagen")
+else:
+    # Aplicar detector de bordes Canny
+    bordeCanny = cv2.Canny(img, 100, 200)
+
+    # Mostrar imágenes
+    cv2.imshow('Original', img)
+    cv2.imshow('Borde Canny', bordeCanny)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
